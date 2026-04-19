046c1e48a7954156e828766dd6c5f7d2d4d2d4e1
Kiri Lefebvre <klefebvr@myune.edu.au>
2026-04-19 21:29:06 +1000

    CSS cleanup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,24 @@ import { Button } from '../components'
 
 import SingleFileUploader from '../components/SingleFileUploader'
 
+import style from './Home.module.css'
+
 export default function Home() {
 
     return (
         <>
-          <div>Home</div>
-          <Button>Test Button</Button>
-          <h1>File Upload:</h1>
-          <SingleFileUploader/>
-           
+          <div class={style.intro_box}>
+            <h1>Welcome to Badminton Stroke Classifier</h1>
+            <h2>Browse videos or upload a video to begin</h2>
+            <Button>Upload Video</Button>
+          </div>
+          <div class={style.upload_box}>
+            <p>File Upload:</p>
+            <SingleFileUploader/>
+          </div>
+          <div class={style.browse_box}>
+
+          </div>
          </>
     )
 }
