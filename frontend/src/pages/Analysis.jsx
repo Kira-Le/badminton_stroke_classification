de7de5bc7e1e6304203cf74fd36435bbe2c358b4
Kiri Lefebvre <klefebvr@myune.edu.au>
2026-04-27 15:20:51 +1000

    Refine Analysis page before adding crop components

--- frontend/src/pages/Analysis.jsx
+++ frontend/src/pages/Analysis.jsx
@@ -5,32 +5,28 @@ import { Button, RadioButtonGroup } from '../components'
 
 import style from './Analysis.module.css'
 
-import video from '../assets/file_example_MP4_1920_18MG.mp4'
+import video from '../assets/file_example_MP4_1920_18MG.mp4' // For testing stages only
 
-const API_BASE = 'http://127.0.0.1:8000'
+const API_BASE = 'http://127.0.0.1:8000' // For testing stages only
 
 export default function Analysis() {
     const [models, setModels] = useState([])
+    const[selectedValue, setSelectedValue] = useState('')
     const [status, setStatus] = useState('')
     const navigate = useNavigate()
     
-    // Run once on page load, to get available models
+    // Run once on page load, to get available models and set initial selection to the first model
     useEffect(() => {
-        fetch(`${API_BASE}/api/models`)
+        fetch(`${API_BASE}/api/models`) // TODO: Replace API_BASE with production URL
               .then((response) => response.json())
-              .then((json) => setModels(json.models))
+              .then((json) => {
+                setModels(json.models)
+                setSelectedValue(json.models[0])
+              })
               .catch((error) => console.error('Error fetching data: ', error))
     }, [])
-
-    // Radio Button Group set up 
-    const[selectedValue, setSelectedValue] = useState('')
-
-    useEffect(() => {
-        if (models.length > 0) {
-            setSelectedValue(models[0])
-        }
-    }, [models])
-
+    
+    // Radio button click handling function
     function radioGroupHandler(event) {
         setSelectedValue(event.target.value)
     }
@@ -38,11 +34,10 @@ export default function Analysis() {
     // Get status of processing task
     useEffect(() => {
         const interval = setInterval(() => {
-            fetch(`${API_BASE}/api/status/123`)
+            fetch(`${API_BASE}/api/status/123`) // TODO: Replace API_BASE with production url and 123 with real job ID
               .then((response) => response.json())
               .then((json) => {
                 setStatus(json.status)
-                console.log(json.status)
                 if (json.status === "complete") {
                     clearInterval(interval) // stop polling
                     navigate("/results")
@@ -51,15 +46,13 @@ export default function Analysis() {
               .catch((error) => console.error('Error fetching data: ', error))
         }, 2000)
         return () => clearInterval(interval)
-    }, [])
+    }, [navigate])
 
     return (
         <>
           <h1>Analysis</h1>
           <div className={style.layout}>
-            <div className={style.video_player}>
-              <video controls width="100%" className={style.videoPlayer} src={video}></video>
-            </div>
+            <video controls className={style.videoPlayer} src={video} />
             <div className={style.input_group}>
               <Button>Identify court</Button>
               <Button>Identify target player</Button>
@@ -73,7 +66,7 @@ export default function Analysis() {
               />
               }
               <Button>Classify stroke</Button>
-              <div>Stroke classification is: {status}</div>
+              {status && <div>Stroke classification is: {status}</div>}
             </div>
           </div>
         </>
